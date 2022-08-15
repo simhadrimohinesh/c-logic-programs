fef3344f45146a5f59bd86ab7509a952ebd1ffc7
simhadrimohinesh <110514679+simhadrimohinesh@users.noreply.github.com>
2022-08-15 20:54:39 +0530

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    int i,j;
+    int size=(2*n)-1;
+    int start=0,end=size-1;
+    int a[size][size];
+    while(n>0)
+    {
+        for(i=start;i<=end;i++)
+        {
+            for(j=start;j<=end;j++)
+            {
+        if(i==start||j==start||i==end||j==end)
+            a[i][j]=n;
+            }
+        }
+    start++;
+    end--;
+    n--;
+    }
+    for(i=0;i<size;i++)
+    {
+        for(j=0;j<size;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
