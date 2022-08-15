f6aacab751eb8717ef30905a0661ad5eb734db57
simhadrimohinesh <110514679+simhadrimohinesh@users.noreply.github.com>
2022-08-15 21:06:20 +0530

    Add files via upload

---
+++ ternary operator.c
@@ -0,0 +1,9 @@
+#include<stdiio.h>
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    (n>0)?printf("positive"):(n<0)printf("negative"):printf("zero");
+    return 0;
+}
