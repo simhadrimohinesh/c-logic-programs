866b6740821556231e67e57c75ccad73012f43b9
simhadrimohinesh <110514679+simhadrimohinesh@users.noreply.github.com>
2022-08-27 21:00:55 +0530

    Add files via upload

---
+++ subsequence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[50];
+    scanf("%s",s);
+    int i,j,k;
+    int len=strlen(s);
+    for(i=1;i<=len;i++)
+    {
+        for(j=0;j)
+        {
+            for(k)
+        }
+    }
+    return 0;
+}
