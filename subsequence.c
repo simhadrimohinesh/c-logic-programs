d2700f626af874ccdaf3f166529f5948e04d8f35
simhadrimohinesh <110514679+simhadrimohinesh@users.noreply.github.com>
2022-08-28 10:01:39 +0530

    Update subsequence.c

--- subsequence.c
+++ subsequence.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
 #include<string.h>
+void substring(char str[],int n)
+{
+int x,i,k,j;
+for(i=0;i<=n;i++)
+{
+for(j=0;j<n-i;j++)
+{
+x=j+i;
+for(k=j;k<=x;k++)
+{
+printf("%c",str[k]);
+}
+printf("\n");
+}
+}
+}
 int main()
 {
-    char s[50];
-    scanf("%s",s);
-    int i,j,k;
-    int len=strlen(s);
-    for(i=1;i<=len;i++)
-    {
-        for(j=0;j)
-        {
-            for(k)
-        }
-    }
-    return 0;
+char str[]="abcd";
+substring(str,strlen(str));
+return 0;
 }
